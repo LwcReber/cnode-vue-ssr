cd69247fe62d144fc5e1c8f19c5e94962ac7f6fe
LwcReber <1079670310@qq.com>
2018-12-21 22:44:22 +0800

    添加接口

--- server/db/db.js
+++ server/db/db.js
@@ -23,41 +23,57 @@ const handleRequest = ({status, data, ...rest}) => {
 
 module.exports = () => {
   return {
-    async getAllTodos (url) {
-      return handleRequest(await request.get(url))
+    async getTopics ({page, tab, limit, mdrender}) {
+      return handleRequest(await request.get('/topics', {params: {page, tab, limit, mdrender: mdrender || false}}))
+    },
+    async getTopicsDetail ({id}) {
+      return handleRequest(await request.get(`/topic/${id}`, {params: {mdrender: false, accesstoken: ''}}))
+    },
+    async createTopics (params) {
+      // 把主题归类写死为dev的测试环境
+      return handleRequest(await request.post('/topics', {...params, tab: 'dev'}))
+    },
+    async collectTopic (params) {
+      return handleRequest(await request.post('/topic_collect/collect', params))
+    },
+    async uncollectTopic (params) {
+      return handleRequest(await request.post('/topic_collect/de_collect', params))
+    },
+    async getCollectTopics ({loginname}) {
+      return handleRequest(await request.get(`/topic_collect/${loginname}`))
+    },
+    async createReplies (params) {
+      let para = {accesstoken: '', content: ''}
+      const {topic_id, accesstoken, content} = params
+      para.accesstoken = accesstoken
+      para.content = content
+      // 如果回复的是别人的评论，则请求添加reply_id属性
+      if (params.reply_id) {
+        para.reply_id = params.reply_id
+      }
+      return handleRequest(await request.post(`/topic/${topic_id}/replies`, para))
+    },
+    async repliesAction (params) {
+      const {reply_id, accesstoken} = params
+      return handleRequest(await request.post(`/reply/${reply_id}/ups`, {accesstoken}))
+    },
+    async getUser (params) {
+      return handleRequest(await request.get(`/user/${loginname}`))
+    },
+    async checkUser (params) {
+      return handleRequest(await request.post('/accesstoken', params))
+    },
+    async getMsgCount ({accesstoken}) {
+      return handleRequest(await request.get(`/message/count`, {params: {accesstoken}}))
+    },
+    async getAllMsg ({accesstoken, mdrender}) {
+      return handleRequest(await request.get('/messages', {params: {accesstoken, mdrender: mdrender || true}}))
+    },
+    async msgMarkAll(params) {
+      return handleRequest(await request.post('/message/mark_all', params))
+    },
+    async msgMarkOne({msg_id, accesstoken}) {
+      return handleRequest(await request.post(`/message/mark_one/${msg_id}`, {accesstoken}))
     }
-    // async addTodo (todo) {
-    //   return handleRequest(await request.post(
-    //     `/${className}`,
-    //     todo,
-    //     {headers: getHeadders()}
-    //   ))
-    // },
-    // async updateTodo (id, todo) {
-    //   return handleRequest(await request.put(
-    //     `/${className}/${id}`,
-    //     todo,
-    //     {headers: getHeadders()}
-    //   ))
-    // },
-    // async deleteTodo (id) {
-    //   return handleRequest(await request.delete(
-    //     `/${className}/${id}`,
-    //     {headers: getHeadders()}
-    //   ))
-    // },
-    // async deleteComleted (ids) {
-    //   const requests = ids.map(id => {
-    //     return {
-    //       method: 'DELETE',
-    //       path: `/mcm/api/${className}/${id}`
-    //     }
-    //   })
-    //   return handleRequest(await request.post(
-    //     '/batch', // 批量处理多个请求
-    //     {requests},
-    //     {headers: getHeadders()}
-    //   ))
-    // }
   }
 }
--- server/routers/api.js
+++ server/routers/api.js
@@ -1,50 +1,101 @@
 // 定义api接口
 const Router = require('koa-router')
 
-const apiRouter = new Router({prefix: '/api'})
+const apiRouter = new Router()
 
-const validateUser = async (ctx, next) => {
-  if (!ctx.session.user) {
-    ctx.status = 401
-    ctx.body = 'new login'
-  } else {
-    await next()
-  }
-}
-// 所有请求都添加请求登录校验
-apiRouter.use(validateUser)
-
-// const successResponse = (data) => {
-//   return {
-//     success: true,
-//     data
+// const validateUser = async (ctx, next) => {
+//   if (!ctx.session.user) {
+//     ctx.status = 401
+//     ctx.body = 'new login'
+//   } else {
+//     await next()
 //   }
 // }
+// // 所有请求都添加请求登录校验
+// apiRouter.use(validateUser)
+
+const successResponse = (data) => {
+  return data
+}
 
 apiRouter
-  .get('/topics/:page/:tab/:limit', async (ctx) => {
-    console.log(ctx.params, '----------------------------')
-    // const todos = await ctx.db.getAllTodos('/topics/:page/:tab/:limit')
-    // ctx.body = successResponse(todos)
+  // 获取主题列表
+  .get('/topics/:page/:tab/:limit/:mdrender?', async (ctx) => {
+    const {params} = ctx
+    const topics = await ctx.db.getTopics(params)
+    ctx.body = successResponse(topics)
   })
-  // .post('/todo', async (ctx) => {
-  //   const data = await ctx.db.addTodo(ctx.request.body)
-  //   ctx.body = successResponse(data)
-  // })
-  // // 修改
-  // .put('/todo/:id', async (ctx) => {
-  //   const data = await ctx.db.updateTodo(ctx.params.id, ctx.request.body)
-  //   ctx.body = successResponse(data)
-  // })
-  // // 删除
-  // .delete('/todo/:id', async (ctx) => {
-  //   const data = await ctx.db.deleteTodo(ctx.params.id)
-  //   ctx.body = successResponse(data)
-  // })
-  // // 删除所有
-  // .post('/delete/completed', async (ctx) => {
-  //   const data = await ctx.db.deleteComleted(ctx.request.body.ids)
-  //   ctx.body = successResponse(data)
-  // })
+  // 获取主题详情
+  .get('/topic/:id/:mdrender?/:accesstoken?', async (ctx) => {
+    const {params} = ctx
+    const topic = await ctx.db.getTopicsDetail(params)
+    ctx.body = successResponse(topic)
+  })
+  // 新建主题
+  .post('/create/topic', async (ctx) => {
+    const data = await ctx.db.createTopics(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 收藏主题
+  .post('/collect/topic', async (ctx) => {
+    const data = await ctx.db.collectTopic(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 取消收藏主题
+  .post('/uncollect/topic', async (ctx) => {
+    const data = await ctx.db.uncollectTopic(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 获取用户收藏的主题
+  .get('/getCollects/:loginname', async (ctx) => {
+    const {params} = ctx
+    const topics = await ctx.db.getCollectTopics(params)
+    ctx.body = successResponse(topics)
+  })
+  // 新建评论
+  .post('/createReplies', async (ctx) => {
+    // 请求数据格式 {topic_id, accesstoken, content，reply_id} 没有reply_id时，发空
+    const data = await ctx.db.createReplies(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 为评论点赞或者取消点赞
+  .post('/repliesAction', async (ctx) => {
+    const data = await ctx.db.repliesAction(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 用户详情
+  .get('/getUser/:loginname', async (ctx) => {
+    const {params} = ctx
+    const topics = await ctx.db.getUser(params)
+    ctx.body = successResponse(topics)
+  })
+  // 校验用户的accesstoken, 用于登录
+  .post('/checkUser', async (ctx) => {
+    const data = await ctx.db.checkUser(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 获取未读消息数
+  .get('/getMsgCount/:accesstoken', async (ctx) => {
+    const {params} = ctx
+    const data = await ctx.db.getMsgCount(params)
+    ctx.body = successResponse(data)
+  })
+  // 获取已读和未读消息
+  .get('/getAllMsg/:accesstoken/:mdrender?', async (ctx) => {
+    const {params} = ctx
+    const data = await ctx.db.getAllMsg(params)
+    ctx.body = successResponse(data)
+  })
+  // 标记全部消息为已读
+  .post('/message/mark_all', async (ctx) => {
+    const data = await ctx.db.msgMarkAll(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+  // 标记单个消息为已读
+  .post('/message/mark_one', async (ctx) => {
+    const data = await ctx.db.msgMarkOne(ctx.request.body)
+    ctx.body = successResponse(data)
+  })
+
 
 module.exports = apiRouter
